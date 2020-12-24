255fd603f16e23a78019d42285e26ebb95e6caf4
tsah <tsah.weiss@gmail.com>
2020-12-24 19:46:00 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
   name='fython',
-  version='0.0.2',
+  version='0.0.3',
   license='MIT',
   packages=['fython'],
   description='Functional utilities for Python, inspired by Scala',
